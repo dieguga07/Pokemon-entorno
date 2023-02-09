ab93d02cfc5c69226794230c81bf7c02c7f245cb
Diego Gutierrez Vazquez <dgutvaz0702@g.educaand.es>
2023-02-09 16:41:03 +0100

    resolucion de ticket 003

--- src/main/kotlin/Pokemon.kt
+++ src/main/kotlin/Pokemon.kt
@@ -1,10 +1,30 @@
 class Pokemon (id:Int,nombre:String,vida:Int,tipo:String){
 
+    var id:Int = 0
+        set(value) {
+            if(value<0) {
+                throw Exception("El ID no debe ser 0")
+            }
+        }
+
+    var nombre:String = " "
+    var vida:Int = 0
+    var tipo:String  = " "
+
+    init {
+        this.id = id
+      this.vida =vida
+      this.tipo = tipo
+        }
+
+    override fun toString(): String {
+        return "Hola  mi nombre es'$nombre' con $id vida=$vida y  tipo='$tipo'"
+    }
+}
 
 
 
 
 
 
-    
 }
