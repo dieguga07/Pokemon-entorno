3bc48b821680607cea8c1debbb879405cbb2038e
Diego Gutierrez Vazquez <dgutvaz0702@g.educaand.es>
2023-02-16 17:02:09 +0100

    Realizacion del ticket AA0002

--- src/main/kotlin/Tipo.kt
+++ src/main/kotlin/Tipo.kt
@@ -2,6 +2,22 @@ import java.lang.IllegalArgumentException
 
 class Tipo(tipos:String) {
 
+    var lista_tipos = mutableListOf<String>("acero","agua", "bicho", "dragon", "electrico", "fantasma", "fuego", "hada", "hielo", "lucha",
+        "normal", "planta", "psiquico", "roca", "siniestro", "tierra", "veneno", "volador")
+    var tipo = ""
+        get() {
+            return field
+        }
+        set(value) {
+            if (value in lista_tipos){
+                field = value
+            }else{
+                throw IllegalArgumentException("Tipo no valido")
+            }
+        }
+    init {
+        tipo = tipos
+    }
 
 
 
